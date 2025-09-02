b4bb185f8b06ae63d0499942b443071aad5e412e
koreamax <129860954+koreamax@users.noreply.github.com>
2025-09-02 20:31:18 +0900

    [level 2] Title: 땅따먹기, Time: 27.55 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int f = 0;
+        int[] temp = new int[4];
+        
+        for (int i = 0; i < land.length - 1; i++) {
+            // 에잇 더럽다 더러워
+            for (int j = 0; j < 4; j++) {
+                temp[j] = land[i][j];
+            }
+            Arrays.sort(temp); // temp 정렬
+            for (int j = 0; j < 4; j++) { // temp max 값과 같은 인덱스 찾기
+                if (temp[3] == land[i][j]) {
+                    f = j;
+                }
+            }
+            for (int j = 0; j < 4; j++) {
+                if (f == j) { // 최댓값 인덱스와 같다면 두 번째 최댓값 더하기
+                    land[i + 1][j] += temp[2];
+                }
+                else { // 최대값 인덱스와 같지 않다면 최댓값 더하기
+                    land[i + 1][j] += temp[3];
+                }
+            }
+        }
+        
+        Arrays.sort(land[land.length - 1]);
+        return land[land.length - 1][3];
+    }
+}
