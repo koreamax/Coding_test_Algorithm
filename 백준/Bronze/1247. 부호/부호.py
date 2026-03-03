1978844934ad593f561558104bd3ca9788775817
koreamax <129860954+koreamax@users.noreply.github.com>
2026-03-03 16:48:11 +0900

    [Bronze III] Title: 부호, Time: 172 ms, Memory: 44600 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.py
+++ 백준/Bronze/1247. 부호/부호.py
@@ -1,23 +1,12 @@
-max_int = 9223372036854775807
+from sys import stdin
 
-for i in range(3):
-    sum = 0
-    cnt = 0
-    N = int(input())
-    for j in range(N):
-        sum += int(input())
-        if (sum >= max_int):
-            sum -= max_int
-            cnt += 1
-        elif (sum <= -max_int):
-            sum += max_int
-            cnt -= 1
+for _ in range (3):
+    N = int(stdin.readline())
+    li = [int(stdin.readline()) for _ in range(N)]
 
-    sum += cnt * max_int
-
-    if sum > 0:
+    if sum(li) > 0:
         print("+")
-    elif sum < 0:
+    elif sum(li) < 0:
         print("-")
     else:
-        print("0")
+        print(0)
