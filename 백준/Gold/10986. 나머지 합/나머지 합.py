53e96522fa09e9f1966156f79761ecff51bce5a5
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-03-12 23:33:42 +0900

    [Gold III] Title: 나머지 합, Time: 536 ms, Memory: 157276 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -4,23 +4,18 @@ input = sys.stdin.readline
 
 N , M = map(int, input().split())
 arr = list(map(int, input().split()))
-cnt = 0 # M으로 나누어 떨어지는 구간의 개수
-dp = [arr[0]]
-remainder = [0 for _ in range(M)]
-remainder[dp[-1] % M] += 1
+dp = 0 #dp는 더하고 끝낼거라 배열이 필요없음
+remainder = [0 for _ in range(M)] # 나머지 값 저장
 
-# 부분 합 알고리즘, O(n) = 1000000
-for i in range(N-1):
-    # (a + b) % c = ((a % c) + (b % c)) % c -> 메모리 용량 줄이기
-    dp.append(((arr[i+1] % M) + (dp[i] % M)) % M)
-    remainder[dp[-1] % M] += 1
+for i in range(N):
+    dp += arr[i] # 구간 합 알고리즘
+    remainder[dp % M] += 1 # 나머지 값에 1추가
 
-# 나머지가 0인 것들
-cnt += remainder[0]
+cnt = remainder[0] # cnt는 구간의 합이 M으로 나눠지는 것들
 
-# O(n) = 1000
 for i in range(M):
-    if remainder[i] >= 2:
-        cnt += (remainder[i] * (remainder[i] - 1)) // 2
+    # 0과 1은 들어가면 안 되지만 어차피 0이 되기 때문에 들어감
+    # nC2
+    cnt += (remainder[i] * (remainder[i] - 1)) // 2
 
 print(cnt)
