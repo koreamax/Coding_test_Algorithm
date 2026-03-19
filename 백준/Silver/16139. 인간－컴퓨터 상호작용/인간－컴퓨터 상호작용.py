35c684b0952b0bfcab8b6227fd0466f07d84d7b7
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-03-20 00:42:14 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 320 ms, Memory: 162476 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -4,13 +4,14 @@ input = sys.stdin.readline
 
 S = list(input().strip())
 q = int(input())
+# 알파벳마다 구간합 받을 수 있게 만들기
 arr = [[0 for _ in range(len(S))] for _ in range(26)]
 
 for i in range(26):
-    for j in range(len(S)):
+    for j in range(len(S)): # arr 채우기
         if ord(S[j]) == 97 + i:
             arr[i][j] = 1
-    for j in range(len(S)-1):
+    for j in range(len(S)-1): # 구간합 만들기
         arr[i][j+1] += arr[i][j]
 
 for _ in range(q):
