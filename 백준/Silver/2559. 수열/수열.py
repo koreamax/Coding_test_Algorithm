e064cc356a126b1cb03f7613df013eaf7f0c0a7d
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-03-27 04:02:18 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -6,6 +6,7 @@ N, K = map(int, input().split())
 T = list(map(int, input().split()))
 s = [sum(T[:K])] # 슬라이딩 윈도우 만큼의 합 배열
 
+# 윈도우 사이즈보다 큰 값을 넣어야 하니 인덱스 잘 생각할 것
 for i in range(N-K):
     s.append(s[-1]-T[i]+T[K+i])
 
