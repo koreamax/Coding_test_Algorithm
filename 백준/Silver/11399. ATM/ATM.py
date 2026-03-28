fdd236dfae37bfb37ad63f60d2316af208e8aad9
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-03-28 12:17:16 +0900

    [Silver IV] Title: ATM, Time: 196 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,12 +1,30 @@
+# 삽입정렬 연습해보기
+
 import sys
 
 input = sys.stdin.readline
 
 N = int(input())
 P = list(map(int, input().split()))
-P.sort()
+s = [0] * N
+s[0] = P[0]
+
+for i in range(1, N):
+    valid = False
+    for j in range(i-1, -1, -1):
+        if P[i] > s[j]:
+            valid = True
+            break
+    if valid:
+        for k in range(len(s)-1, j+1, -1):
+            s[k] = s[k-1]
+        s[j+1] = P[i]
+    else:
+        for k in range(len(s)-1, -1, -1):
+            s[k] = s[k-1]
+        s[0] = P[i]
 
 for i in range(N-1):
-    P[i+1] += P[i]
+    s[i+1] += s[i]
 
-print(sum(P))
+print(sum(s))
