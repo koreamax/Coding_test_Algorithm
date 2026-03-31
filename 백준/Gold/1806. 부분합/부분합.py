3b9f61b75b64a4d0ea66fb7bf62833eed19cdf0b
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-04-01 08:06:20 +0900

    [Gold IV] Title: 부분합, Time: 100 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -3,18 +3,16 @@ import sys
 input = sys.stdin.readline
 
 N, S = map(int, input().split())
-# O(nlogn)
 arr = list(map(int, input().split()))
 
 
 start, end = 0, 0 # 투 포인터
 m = N + 1 # 최소길이
-sum = arr[0]
+sum = arr[0] # 윈도우 합
 
 while (True): # end가 N까지 갔다는건 이제 끝이라는 것
     if (sum >= S): # sum이 더 크다면 길이 비교
-        if (end - start + 1 < m): # 길이가 최소값이라면 바꿔주기
-            m = end - start + 1
+        m = min(end - start + 1, m) # 길이가 최소값이라면 바꿔주기
         start += 1
         sum -= arr[start-1]
     else: # sum이 더 작으면 end를 더해주기
