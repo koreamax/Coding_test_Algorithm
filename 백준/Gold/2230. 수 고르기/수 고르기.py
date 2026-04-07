07c0c87b27dffb58ab6ddc4f8ba7ed548d9007b3
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-04-07 18:40:30 +0900

    [Gold V] Title: 수 고르기, Time: 184 ms, Memory: 37048 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = sorted([int(input()) for _ in range(N)]) # O(nlogn)
+
+start, end = 0, 1 # 투 포인터
+minimum = sys.maxsize # 최소 사이즈 
+
+while (start < N-1):
+    tmp = A[end] - A[start] # 현재 차
+    
+    if end == N-1: # end가 끝까지 갔으면 start만 추가
+        start += 1
+    else:
+        if tmp < M: # 차가 M 보다 작으면 end를 늘려주기
+            end += 1
+        else: # 차가 M 보다 크면
+            if start == end: # start와 end가 같으면 end를 늘려주고
+                end += 1
+            else: # 아니라면 start만 추가
+                start += 1
+    
+    if tmp > M: # 최솟값 비교
+        minimum = min(tmp, minimum)
+    elif tmp == M:
+        minimum = M
+        break
+
+print(minimum)
