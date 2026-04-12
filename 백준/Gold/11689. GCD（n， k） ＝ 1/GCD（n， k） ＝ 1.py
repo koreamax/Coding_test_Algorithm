3d1fddae53fcb29cc6d1b4d5166b8aba7a7a8979
LEEMINHYUNG <129860954+koreamax@users.noreply.github.com>
2026-04-13 02:56:56 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -9,10 +9,10 @@ answer = n
 
 i = 2
 # O(n**0.5) = 10**6
-for i in range(2, int(n**(0.5)) + 1):
+for i in range(2, int(n**(0.5)) + 1): # 소인수분해
     while n % i == 0:
         n //= i
-        if i not in prime:
+        if i not in prime: # 굳이 있으면 넣지 않음
             prime.append(i)
 
 if n != 1: # n = 1일 때 넣을 필요 없음
